e3937edb606fc0d5513a7a6ff2aa50f3dfb8fc3d
code-cube-lab <289831990+code-cube-lab@users.noreply.github.com>
2026-07-30 18:28:06 +0300

    Обновить клиентский сайт до v19

---
+++ app/bath/page.tsx
@@ -0,0 +1,5 @@
+import { ProjectV18 } from "../ProjectV18";
+
+export default function BathPage() {
+  return <ProjectV18 page="bath" />;
+}
---
+++ app/landscape/page.tsx
@@ -0,0 +1,5 @@
+import { ProjectV18 } from "../ProjectV18";
+
+export default function LandscapePage() {
+  return <ProjectV18 page="landscape" />;
+}
